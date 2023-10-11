88c10dc7a2a594bd9636fc25f2fa23b5b6201bd0
Jesse Dufrene <jessecdufrene@gmail.com>
2023-10-10 18:36:44 -0600

    split out some condensed code
    this way, it's easier to understand what's being done

--- mintotp.py
+++ mintotp.py
@@ -8,16 +8,18 @@ import time
 
 
 def hotp(key, counter, digits=6, digest='sha1'):
-    key = base64.b32decode(key.upper() + '=' * ((8 - len(key)) % 8))
+    key_padded = key.upper() + '=' * ((8 - len(key)) % 8)
+    key_decoded = base64.b32decode(key_padded)
     counter = struct.pack('>Q', counter)
-    mac = hmac.new(key, counter, digest).digest()
+    mac = hmac.new(key_decoded, counter, digest).digest()
     offset = mac[-1] & 0x0f
     binary = struct.unpack('>L', mac[offset:offset+4])[0] & 0x7fffffff
     return str(binary)[-digits:].zfill(digits)
 
 
 def totp(key, time_step=30, digits=6, digest='sha1'):
-    return hotp(key, int(time.time() / time_step), digits, digest)
+    counter = int(time.time() / time_step)
+    return hotp(key, counter, digits, digest)
 
 
 def main(key=None):
